a49182cdcae81456cd424a3e26c8144eb9778ac8
Abhishekh Sivakumar <abhishekh.sivakumar@students.iiit.ac.in>
2022-08-03 09:39:06 +0530

    begin index

---
+++ indexer/indexer.py
@@ -0,0 +1,11 @@
+class Indexer:
+    """Indexes the corpus using inverted indexing"""
+
+    def __init__(self):
+        self.index = {}
+
+    def gen_from_file(self, doc_list):
+        """Given file with list of documents, construct inverted index
+        :param doc_list: path to file containing file names of documents
+        """
+        pass
