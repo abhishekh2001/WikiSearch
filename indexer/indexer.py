03eb3d2591f7316b7276efa8e17b5c216f47ab5b
Abhishekh Sivakumar <abhishekh.sivakumar@students.iiit.ac.in>
2022-08-04 00:08:09 +0530

    structure of indexing

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -1,11 +1,31 @@
 class Indexer:
     """Indexes the corpus using inverted indexing"""
 
-    def __init__(self):
-        self.index = {}
-
-    def gen_from_file(self, doc_list):
-        """Given file with list of documents, construct inverted index
-        :param doc_list: path to file containing file names of documents
+    def __init__(self, fpath):
+        """Creates an inverted index from XML document
+        :param fpath: path to XML document containing corpus
         """
+
+        self.index = {}
+        self.fpath = fpath
+        self.doc = None
+
+    def parse(self):
+        """Parses the corpus"""
+        pass
+
+    def tokenize(self, t):
+        """Performs tokenization"""
+        pass
+
+    def cfold(self, t):
+        """Performs case folding"""
+        pass
+
+    def remove_stop_words(self, t):
+        """removes stop words"""
+        pass
+
+    def stem(self, t):
+        """Performs stemming"""
         pass
